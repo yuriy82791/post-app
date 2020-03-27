6cacc23ad483a27a899ecaa8abcfc0b1260ca700
yuriy82791 <yuriy82791@gmail.com>
2020-03-27 17:47:33 +0200

    Updates

---
+++ precache-manifest.39bc8128653653f268bd8f78084307a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce649da52ff63eef953fcb3c25af348b",
+    "url": "/post-app/index.html"
+  },
+  {
+    "revision": "1d49966831b05c2a9aa8",
+    "url": "/post-app/static/css/main.55364070.chunk.css"
+  },
+  {
+    "revision": "864792ef411095c68158",
+    "url": "/post-app/static/js/2.2038d5fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/post-app/static/js/2.2038d5fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d49966831b05c2a9aa8",
+    "url": "/post-app/static/js/main.12b47405.chunk.js"
+  },
+  {
+    "revision": "d366b2cb79f4e86d9f91",
+    "url": "/post-app/static/js/runtime-main.17506c06.js"
+  }
+]);
